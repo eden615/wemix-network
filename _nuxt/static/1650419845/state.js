8a9d333f227d2253bf88c9ecc84155a3d18621f2
eden <eden615@wemade.com>
2022-04-20 11:04:23 +0900

    init

---
+++ _nuxt/static/1650419845/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fwemix-network\u002F_nuxt\u002Fstatic\u002F1650419845",layout:"default",error:a,state:a,serverRendered:false,routePath:"\u002F",config:{_app:{basePath:"\u002Fwemix-network\u002F",assetsPath:"\u002Fwemix-network\u002F_nuxt\u002F",cdnURL:a}}}}(null));
